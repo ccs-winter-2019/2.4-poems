53325ffa6e58324585ef4114321401085b58a268
Dan Dietz <danddietz@gmail.com>
2019-01-17 17:31:55 -0400

    Added custom jinja2 filters

--- app.py
+++ app.py
@@ -4,27 +4,22 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
+from poems import all_poems
+
+
 app = Flask(__name__)
 
-
-all_poems = [
-    {'title': 'The Variable', 'body': 'Some say....'},
-    {'title': 'The List', 'body': 'Some say....'},
-    {'title': 'The Tuple', 'body': 'Some say....'},
-    {'title': 'Dictionaries', 'body': 'Some say....'},
-]
 
 @app.route("/")
 def index():
 
     context = {
         'poems': all_poems,
-        'my_name': 'Dan',
         'favorite_flavor': 'coffee',
     }
 
+    # return render_template('index.html', poems=all_poems, favorite_flavor='coffee')
     return render_template('index.html', **context)
-
 
 @app.route('/poem/<poem_id>/')
 def book_now(poem_id):
@@ -33,4 +28,14 @@ def book_now(poem_id):
 
     poem = all_poems[poem_index]
 
-    return render_template('poem_detail.html', poem=poem)
+    ctx = {
+        'poem': poem,
+    }
+
+    return render_template('poem_detail.html', **ctx)
+
+
+def register_custom_template_filters():
+    import linebreaks
+
+register_custom_template_filters()
---
+++ poems.py
@@ -0,0 +1,37 @@
+"""
+This file holds our poem data. We could fetch this data from a CSV, API, database, etc.
+"""
+
+
+all_poems = [
+    {
+        'title': 'The Variable',
+        'body': """
+            Some say the Variable is meritorious.
+            I say the Variable is mysterious.
+            At assignment, best be serious.
+            Remembering string or number can make you delirious.
+        """
+    },
+    {
+        'title': 'The List',
+        'body': """
+            The List is a storage hero
+            But oddly requires counting from zero
+        """
+    },
+    {
+        'title': 'The Tuple',
+        'body': """
+            If a mighty Tuple you must change
+            Instead delete it and a new one exchange
+        """
+    },
+    {
+        'title': 'Dictionaries',
+        'body': """
+            A Dictionary with its key-value pairs
+            Can avoid for loop musical chairs
+        """
+    },
+]
